aee06dae5f155f464ff36eb45b67cb7ea29ac583
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-05-05 23:42:55 -0400

    close initially open banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // pages
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
