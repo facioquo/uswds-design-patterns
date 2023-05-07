ce5ab88622fb1fd1e031aaf3d8a6ee6e3ea23535
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-05-07 02:51:33 -0400

    add theme settings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  scrollToStart(id: string) {
+    setTimeout(() => {
+      const element = document.getElementById(id);
+      element!.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+    }, 200);
+  }
+
+  scrollToEnd(id: string) {
+    setTimeout(() => {
+      const element = document.getElementById(id);
+      element!.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
+    }, 200);
+  }
 }
+
