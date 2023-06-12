71dfd5c7b8ce04d4900492f9e7cbce086d069ea0
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-06-11 22:04:09 -0400

    fix: remove PWA service worker (#28)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule, Meta } from '@angular/platform-browser';
 import { NgOptimizedImage } from '@angular/common'
 
@@ -9,7 +9,6 @@ import { UtilityService } from './services/utility.service';
 
 import { SiteIdentifierComponent } from './components/site-identifier/site-identifier.component';
 import { SiteBannerComponent } from './components/site-banner/site-banner.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,13 +19,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule,
     NgOptimizedImage,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    AppRoutingModule
   ],
   providers: [
     Meta,
