1679903625a98cd5a479c66e372ff677494594cd
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-02 16:28:54 -0400

    add ghost card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,5 +27,15 @@ export class HomeComponent {
 
   // landing page card data
   public cards: Card[] = CARD_LIST;
+
+  // ghost card for suggestions
+  public suggestionCard: Card = {
+    "id": "ghost-card",
+    "title": "» make a suggestion",
+    "description": "Do you have an ideas to share on our site?  Send us a link or a screenshot.",
+    "link": "https://github.com/facioquo/uswds-design-patterns/issues/new?labels=enhancement&template=feature_request.md",
+    "image": "add",
+    "imageType": "usa-icon"
+  }
 }
 
