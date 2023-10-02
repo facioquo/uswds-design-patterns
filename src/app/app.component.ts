6a314c56f9f80419955918887984e1feaf798445
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-02 02:25:59 -0400

    refactor pattern footer to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-
-
-  ngOnInit(): void {
-
-  }
-}
+export class AppComponent { }
--- src/app/pages/sticky-menu/sticky-menu.module.ts
+++ src/app/pages/sticky-menu/sticky-menu.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PatternFooterComponent } from 'src/app/components/pattern-footer/pattern-footer.component';
 import { StickyMenuRoutingModule } from './sticky-menu-routing.module';
 import { StickyMenuComponent } from './sticky-menu.component';
 
 @NgModule({
   declarations: [
+    PatternFooterComponent,
     StickyMenuComponent
   ],
   imports: [
