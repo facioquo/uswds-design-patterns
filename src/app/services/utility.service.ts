61b43deaca45d1ea68da3151acaec1c713f29b82
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-20 02:45:46 -0400

    chore: add version markers (#91)

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -5,10 +5,10 @@ import { WORD_LIST } from "./utility.model";
 import { Card, PATTERNS } from 'src/app/pages/patterns.model';
 
 const URL_BASE = "https://uswds.facioquo.com";
-const URL_IMAGE_SOCIAL = URL_BASE.concat("/assets/images/social-card.png?v=2023.10.18");
+const URL_IMAGE_SOCIAL = URL_BASE.concat("/assets/images/social-card.png?v=YYYY.MM.DD");
 const SITE_TITLE = "Idea book: design patterns for U.S. Web Design System sites";
 const SITE_DESCRIPTION = "A design pattern idea book for designers and developers using the U.S. Web Design System (USWDS).";
-const VERSION_DATE = "2023.10.18";
+const VERSION_DATE = "YYYY.MM.DD";
 
 export {
   URL_BASE,
